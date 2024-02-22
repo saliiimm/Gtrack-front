5b5209fc94f79f4f6d4f7a8ddf8d868b79fd1d75
berchiche willem <berwillempro@gmail.com>
2024-02-22 23:38:59 +0100

    fixes

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import WorkSpace from "./Pages/WorkSpace/WorkSpace.jsx";
-import LandingPage from "./Pages/LandingPage/LandingPage.jsx"
-import Inbox from "./Pages/Inbox/Inbox.jsx";
+import WorkSpace from "./pages/WorkSpace/WorkSpace.jsx";
+import LandingPage from "./pages/LandingPage/LandingPage.jsx";
+import Inbox from "./pages/Inbox/Inbox.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <LandingPage/>,
+    element: <LandingPage />,
   },
   {
     path: "/WorkSpace",
