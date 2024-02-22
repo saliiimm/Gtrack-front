f5275f00f5bcc4c2015af48e3974ace11131b00c
Ghalem Salim <salimghalem40@gmail.com>
2024-02-22 13:43:10 +0100

    nav changed name

--- src/components/Navbar/Nav.jsx
+++ src/components/Navbar/Nav.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import "./Navbar.css";
+import "./Nav.css";
 import Logo from "../../assets/logoGhack.svg";
 const Navbar = () => {
   const [isScrolled, setIsScrolled] = useState(false);
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,7 +1,7 @@
 import Collaboration from "../components/Collaboration/Collaboration";
 import FriendlyTolls from "../components/FriendlyTools/FriendlyTolls";
 import Home from "../components/Home/Home";
-import Navbar from "../components/Navbar/Navbar";
+import Navbar from "../components/Navbar/Nav";
 import PerfectFit from "../components/PerfectFit/PerfectFit";
 import Footer from "../components/Footer/Footer";
 
