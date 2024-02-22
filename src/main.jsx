5cc200d4b6653c276cc2fc650e476374241d4408
sido200 <93400204+sido200@users.noreply.github.com>
2024-02-22 17:40:22 +0100

    sidali

--- src/main.jsx
+++ src/main.jsx
@@ -2,17 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import WorkSpace from "./pages/WorkSpace/WorkSpace.jsx";
-import LandingPage from "./pages/LandingPage/LandingPage.jsx";
+import WorkSpace from "./Pages/WorkSpace/WorkSpace.jsx";
+import LandingPage from "./Pages/LandingPage/LandingPage.jsx"
+import Inbox from "./Pages/Inbox/Inbox.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <LandingPage />,
+    element: <LandingPage/>,
   },
   {
     path: "/WorkSpace",
     element: <WorkSpace />,
+  },
+  {
+    path: "/Inbox",
+    element: <Inbox />,
   },
   {
     path: "*",
