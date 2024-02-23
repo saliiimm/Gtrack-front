85f673d017898a97874cd934669c7bb77d35d707
Ghalem Salim <salimghalem40@gmail.com>
2024-02-23 22:37:25 +0100

    details done

--- src/components/TopNav/TopNav.jsx
+++ src/components/TopNav/TopNav.jsx
@@ -1,26 +1,26 @@
-import "./TopNav.css"
-import { FiSearch,FiSettings } from "react-icons/fi";
-import Avatar from '@mui/material/Avatar';
-import logo from "../../assets/Logo.png"
+import "./TopNav.css";
+import { FiSearch, FiSettings } from "react-icons/fi";
+import Avatar from "@mui/material/Avatar";
+import Logo from "../../assets/logoGhack.svg";
+import { Link } from "react-router-dom";
 const TopNav = () => {
-    return (
-        <div className="topnav">
-             <div className="logo">
-             <img src={logo} alt="" />
-        
-             </div>
-             <div className="search">
-                <FiSearch/>
-  <input type="search" name="" id="" placeholder="Search ..."/>
-             </div>
-             <div className="profilMenu">
-             <FiSettings/>
-         <Avatar sx={{ bgcolor:"#0066FF" }}>
-            M
-          </Avatar>
-             </div>
+  return (
+    <div className="topnav">
+      <Link to="/">
+        <div className="nav-left logo">
+          <img src={Logo} /> <p>Track</p>
         </div>
-    );
-}
+      </Link>
+      <div className="search">
+        <FiSearch />
+        <input type="search" name="" id="" placeholder="Search ..." />
+      </div>
+      <div className="profilMenu">
+        <FiSettings />
+        <Avatar sx={{ bgcolor: "#0066FF" }}>M</Avatar>
+      </div>
+    </div>
+  );
+};
 
 export default TopNav;
